bb6fba2c89ed6eb64c1f7ba64f60116d6df32f2e
Younes <ylmourab77@gmail.com>
2021-01-21 15:26:48 +0100

    modification template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import  'bootstrap/dist/css/bootstrap.css';
+import Footer from './components/Footer';
 import Section from './components/Section';
 import './style.css';
 
@@ -7,8 +8,10 @@ import './style.css';
 function App() {
   return (
     <div className="App">
+      <Header/>
 
       <Section></Section>
+      <Footer/>
 
 
     </div>
