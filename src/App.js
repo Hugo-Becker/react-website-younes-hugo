b25ff45ebb84c96210aee8d38b7af54fc1269d6e
Hugo <becker.hugo@hotmail.com>
2021-01-21 10:42:04 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { Navbar } from './components'
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
