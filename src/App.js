c2980df9cf4b1229f7477b9c9a1ac055bf273a25
Younes <ylmourab77@gmail.com>
2021-01-21 15:27:37 +0100

    modification template

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import  'bootstrap/dist/css/bootstrap.css';
 import Footer from './components/Footer';
 import Section from './components/Section';
+import Header from './components/Header'
 import './style.css';
 
 
