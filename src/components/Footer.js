db87a63705070684479ddfb0eea83bc91d5c18e2
Hugo <becker.hugo@hotmail.com>
2021-01-22 13:25:48 +0100

    footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,41 @@ import '../css/footer.css'
 function Footer() {
     return (
         <div className='footer'>
-            <h1>Get in touch</h1>
-            <div className='row'>
-                <div className=" half half-left">
-                    <label htmlFor="">Name</label>
-                    <input type="text"/>
-                </div>
-                <div className=" half half-right">
-                    <label htmlFor="">Email</label>
-                    <input type="email" name="email" id=""/>
-                </div>
+            <div className='footer-container'>
+
+                <h3>Get in touch</h3>
+
+                <div className='row'>
+                    <div className=" half half-left">
+                        <label htmlFor="">Name</label>
+                        <input placeholder='Name' type="text"/>
+                    </div>
+                    
+                    <div className=" half half-right">
+                        <label  htmlFor="">Email</label>
+                        <input placeholder='Email'  type="email" name="email" id=""/>
+                    </div>
+                    <div className='message'>
+                        <label htmlFor="">Message</label>
+                        <textarea placeholder='Message' name="" id="" cols="30" rows="10"></textarea>
+
+                    </div>
+                    <button>SEND MESSAGE</button>
+
                 
+                </div>
+                <p>All right reserved to ...</p>
+
+
+
+
+                
+
+                
+                
+
             </div>
-
-
-
+            
             
         </div>
     )
