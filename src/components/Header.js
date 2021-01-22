39fbdace3cab391625234a15468708a7145ad8f7
Younes <ylmourab77@gmail.com>
2021-01-22 14:49:21 +0100

    navbar fait

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,17 @@ import {
 import Element from '../pages/Element';
 import Generic from '../pages/Generic';
 import Home from '../pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css'
   
 
 import '../css/header.css'
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 function Header() {
     return (
         <Router>
       <header>
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -28,7 +30,32 @@ function Header() {
               <Link to="/generic">Generic</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
+        
+
+
+
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+      <Nav.Link href="#link"><Link to="/element">Element</Link></Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
       </header>
       <Switch>
         <Route path="/element">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Section from '../components/Section'
+import Welcome from '../components/Welcome'
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Welcome></Welcome>
             <Section></Section>
             <Footer></Footer>
             
