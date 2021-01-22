226baa8ed301bfa56cf49d7f8c66f7206bfae77b
Hugo <becker.hugo@hotmail.com>
2021-01-22 16:23:56 +0100

    element in progress

--- src/pages/Element.js
+++ src/pages/Element.js
@@ -1,10 +1,82 @@
 import React from 'react'
+import '../css/element.css'
 
 function Element() {
     return (
-        <div>
-            <h1>Element</h1>
-            
+        <div className='elements'>
+            <div className="ele-sct-1">
+                <div className='titre'>
+                    <h1>Elements</h1>   
+                    <p>Lorem ipsum dolor sit amet.</p> 
+                </div>
+                
+                <div className='row mt-4'>
+                    <div className='col-6'>
+                        <h3 className='text-secondary my-5'>What is a Baseline</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo earum facere dolorum minima nemo numquam vel doloribus ipsum. Recusandae, quaerat nemo. Ex, consequatur beatae repellendus error accusantium praesentium laudantium nulla.</p>
+                    </div>
+                    <div className='col-3'>
+                        <h3 className='text-dark my-5'>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo earum facere dolorum minima nemo numquam vel doloribus ipsum. Recusandae, quaerat nemo.</p>                            
+
+                    </div>
+                    <div className='col-3'>
+                        <h3 className='text-dark my-5'>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo earum facere dolorum minima nemo numquam vel doloribus ipsum. Recusandae, quaerat nemo.</p>  
+
+                    </div>
+
+                </div>
+
+            </div>
+            <hr className='my-5'/>
+            <div className="ele-sct-2">
+                <h3>Sample Content</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci labore harum, doloremque, expedita dolore quam blanditiis alias inventore laboriosam pariatur consectetur, fugit dolorem. Voluptate debitis maxime consequuntur omnis vero doloribus.</p>
+                <div className='row'>
+                    <div className='col-6' >
+                        <h3>lore</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, eligendi dolores fuga molestiae ad incidunt quod alias dolor reiciendis facere quam ab provident doloribus, quibusdam tempora consectetur maxime esse ipsum!</p>
+                    </div>
+                    <div className='col-6'>
+                        <h3>lore</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, eligendi dolores fuga molestiae ad incidunt quod alias dolor reiciendis facere quam ab provident doloribus, quibusdam tempora consectetur maxime esse ipsum!</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-4'>
+                        <h3>lorem</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum explicabo exercitationem incidunt commodi. Aliquid nesciunt explicabo praesentium ducimus, vitae fugit soluta at totam deserunt illum labore perferendis rem laborum error!
+                        </p>
+                    </div>
+                    <div className='col-4'>
+                        <h3>lore</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum explicabo exercitationem incidunt commodi. Aliquid nesciunt explicabo praesentium ducimus, vitae fugit soluta at totam deserunt illum labore perferendis rem laborum error!
+                        </p>
+                    </div>
+                    <div className='col-4'>
+                        <h3>lore</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum explicabo exercitationem incidunt commodi. Aliquid nesciunt explicabo praesentium ducimus, vitae fugit soluta at totam deserunt illum labore perferendis rem laborum error!
+                        </p>
+                    </div>
+                </div>
+                
+
+
+            </div>
+            <hr className='my-5'/>
+            <div className='row ele-sct-3'>
+                <div className='col-6'>
+                    <h1>yooo</h1>
+                    <h1>yooo</h1>
+                    <h1>yooo</h1>
+                </div>
+                
+                
+            </div>
         </div>
     )
 }
