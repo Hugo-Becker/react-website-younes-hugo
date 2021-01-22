860db2782c49ccde99fa6ca5cba8003cc7bf511a
Hugo <becker.hugo@hotmail.com>
2021-01-22 11:22:04 +0100

    temp push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../css/footer.css'
 
 function Footer() {
     return (
-        <div>
+        <div className='footer'>
+            <h1>Get in touch</h1>
+            <div className='row'>
+                <div className=" half half-left">
+                    <label htmlFor="">Name</label>
+                    <input type="text"/>
+                </div>
+                <div className=" half half-right">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id=""/>
+                </div>
+                
+            </div>
+
+
+
             
         </div>
     )
